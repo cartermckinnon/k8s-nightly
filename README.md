fe468cab35ff146bf09d6bf765345e32750c7866
Carter McKinnon <cmckinnon@cw-lr0y22qx77-l.localdomain>
2025-10-03 00:48:35 -0700

    fix: GOBIN issues on GHA runners

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Nightly builds of Kubernetes
 
 This project provides nightly builds of Kubernetes, sourced from the upstream project's CI system.
+
+---
+
+## Usage
+
+Container images are hosted on GitHub Container Registry (`ghcr.io`), but you won't see them in the repository's package list.
+This requires adding a label to the images, changing the digest in the process.
+
+The container images are under `ghcr.io/cartermckinnon/k8s-nightly`, for example:
+```
+docker pull ghcr.io/cartermckinnon/k8s-nightly/kube-apiserver
+```
--- hack/capture-images.sh
+++ hack/capture-images.sh
@@ -15,19 +15,27 @@ IMAGES=(
   "kube-scheduler"
 )
 
-IMAGE_TAG=$(curl "https://storage.googleapis.com/k8s-release-dev/ci/k8s-master.txt")
+IMAGE_TAG=$(curl --silent "https://storage.googleapis.com/k8s-release-dev/ci/k8s-master.txt")
 
 # v1.35.0-alpha.0.1050+aa38aeaca28899 -> v1.35.0-alpha.0.1050_aa38aeaca28899
 IMAGE_TAG="${IMAGE_TAG//+/_}"
+
+echo "Capturing image tag: ${IMAGE_TAG}"
+
+BUILD_DIR="${PWD}/.build/"
+mkdir -p "${BUILD_DIR}"
+export GOBIN="${BUILD_DIR}"
+export PATH=$GOBIN:$PATH
 
 go install github.com/google/go-containerregistry/cmd/crane@latest
 
 FAILED_IMAGES=()
 for IMAGE in ${IMAGES[@]}; do
-  if ! crane pull "gcr.io/k8s-staging-ci-images/${IMAGE}:${IMAGE_TAG}" "${IMAGE}.tar"; then
+  IMAGE_TARBALL="${BUILD_DIR}/${IMAGE}.tar"
+  if ! crane pull "gcr.io/k8s-staging-ci-images/${IMAGE}:${IMAGE_TAG}" "${IMAGE_TARBALL}"; then
     echo "Failed to pull image: ${IMAGE}"
     FAILED_IMAGES+=("${IMAGE}")
-  elif ! crane push "${IMAGE}.tar" "${TARGET_REGISTRY}/${IMAGE}:latest"; then
+  elif ! crane push "${IMAGE_TARBALL}" "${TARGET_REGISTRY}/${IMAGE}:latest"; then
     echo "Failed to push image: ${IMAGE}"
     FAILED_IMAGES+=("${IMAGE}")
   else
